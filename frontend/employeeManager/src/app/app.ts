9ac7e02038c0cc696ee716f41a1ddda9823e365f
Bartosz Lewandowicz <bartosz.lewandowicz@gmail.com>
2025-07-12 17:18:14 +0200

    ponowny init frontendu

--- frontend/employeeManager/src/app/app.ts
+++ frontend/employeeManager/src/app/app.ts
@@ -1,13 +1,15 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EmployeeList } from './features/employee/components/employee-list/employee-list';
 
 @Component({
   selector: 'app-root',
-  imports: [EmployeeList],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
 })
 export class App {
   protected title = 'employeeManager';
